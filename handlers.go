ca109731165e486ec6f853bdcad7dfbce6de9db4
tom caruso <carusot42@gmail.com>
2017-04-25 01:08:52 -0400

    implement header writes

--- handlers.go
+++ handlers.go
@@ -14,13 +14,8 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
-    //todos := Todos{
-        //Todo{Name: "Finish this tutorial"},
-        //Todo{Name: "Get a haircut"},
-    //}
     
-    w.Header().Set("Content-Type", "application/json;charset=UTF-8")
-    w.WriteHeader(http.StatusOK)
+    Write200ResponseHeader(w, r)
 
     if err := json.NewEncoder(w).Encode(todos); err != nil {
         panic(err)
@@ -29,15 +24,24 @@ func TodoIndex(w http.ResponseWriter, r *http.Request) {
 
 func TodoShow(w http.ResponseWriter, r *http.Request) {
     vars := mux.Vars(r)
+    var _404 bool
 
     todoId, err := strconv.Atoi(vars["todoId"])
     
     if err != nil {        
-        w.WriteHeader(404)
-        http.NotFound(w, r)
+        _404 = true
     }
     
-    if err := json.NewEncoder(w).Encode(RepoFindTodo(todoId)); err != nil {
-        panic(err)
+    foundTodo, err := RepoFindTodo(todoId)
+    
+    if err != nil { 
+        _404 = true
+    }
+    
+    if !_404 {
+        Write200ResponseHeader(w, r)
+        SendAsJson(w, foundTodo)
+    } else {
+        Write404ResponseHeader(w, r)
     }
 }
